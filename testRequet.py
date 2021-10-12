3f164d17e024c14e4b09ccf2c3c134f0146e1aa3
admin1337 <nedanon@outlook.com>
2021-10-12 21:06:27 +0300

    working join and limit

--- testRequet.py
+++ testRequet.py
@@ -16,19 +16,21 @@ base_url='http://rtpiapi.hrdn.io/'
 
 request_headers = {'Authorization': f'Bearer {token}',
                     'Content-Type': 'application/json',
-                    'Range-Unit': 'items',
-                    'Range' : '0-1',
-                    'Prefer' : 'count=exact'
+                    'Range-Unit': 'items'
                   }
 
 #Рабочий, но join не работает
-request_url = f"{base_url}rtpi_price_page?and=(date_last_in_stock.gte.2021-09-01, rosstat_id.not.is.null)&rtpi_price(*)?stock_status.eq.InStok"
+#request_url = f"{base_url}rtpi_price_page?and=(date_last_in_stock.gte.2021-09-01, rosstat_id.not.is.null)&rtpi_price(*)?stock_status.eq.InStok"
 
 #request_url = f"{base_url}rtpi_price_page?and=(date_last_in_stock.gte.2020-07-01, date_last_in_stock.lte.2020-09-01, rosstat_id.not.is.null),rtpi_price(stock_status)&rtpi_price.stock_status.eq.InStok"
 
 #request_url = f"{base_url}rtpi_product_name?select=product_name,rtpi_price_page(date_last_in_stock, rosstat_id)and=(date_last_in_stock.gte.2021-09-01, rosstat_id.not.is.null),rtpi_price(stock_status)&stock_status.eq.InStok"
 
 #request_url = f"{base_url}rtpi_product_name?select=product_name,web_price_id:web_price_id(date_last_in_stock, rosstat_id)and=(date_last_in_stock.gte.2021-09-01, rosstat_id.not.is.null)"
+
+request_url = f'{base_url}rtpi_price_page?limit=10&offset=30&' \
+f'select=*,rtpi_product_name(product_name)&' \
+f'date_last_in_stock.gte.2021-10-06'
 
 response = requests.get(request_url, headers=request_headers)
 
