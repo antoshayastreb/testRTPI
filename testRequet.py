47800c6a56cdded05532e46c02dcf8c027bcad5f
Evgeniy <nedanon@outlook.com>
2021-10-16 15:40:01 +0300

    request_fix

--- testRequet.py
+++ testRequet.py
@@ -28,18 +28,21 @@ request_headers = {'Authorization': f'Bearer {token}',
 
 #request_url = f"{base_url}rtpi_product_name?select=product_name,web_price_id:web_price_id(date_last_in_stock, rosstat_id)and=(date_last_in_stock.gte.2021-09-01, rosstat_id.not.is.null)"
 
-request_url = f'{base_url}rtpi_price_page?limit=10&offset=30&' \
+request_url = f'{base_url}rtpi_price_page?' \
 f'select=*,rtpi_product_name(product_name)&' \
-f'date_last_in_stock.gte.2021-10-06'
+f'date_last_in_stock=gte.2021-10-06' 
+#f'&rtpi_price_page.order=date_last_in_stock'
 
 response = requests.get(request_url, headers=request_headers)
 
 #decoded_content = response.content.decode('utf-8')
 
 decoded_content = response.json()
-  
-pdObj = pd.DataFrame(decoded_content, columns=["web_price_id", "price_name", "price_url", "date_add", "date_last_in_stock", "rosstat_id", "contributor_id", "store_id"])
 
-pdObj.to_csv('list.csv', sep = "\t", index=False)
-#pdObj.to_excel('list.xlsx', engine='xlsxwriter')
+if (response.status_code // 100 != 2):
+  print(decoded_content)
+else:
+  pdObj = pd.DataFrame(decoded_content, columns=["web_price_id", "rtpi_product_name", "price_url", "date_add", "date_last_in_stock", "rosstat_id", "contributor_id", "store_id"])
+  pdObj.to_csv('list.csv', sep = "\t", index=False)
+  #pdObj.to_excel('list.xlsx', engine='xlsxwriter')
   
